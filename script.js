fe70d40fb4da0734c774e06107b811505a49b2df
yofukashino <alokender018@gmail.com>
2024-12-03 17:55:27 +0530

    works

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
-const dropArea = document.getElementById('drop-area');
-
-
-document.body.addEventListener('paste', (event) => {
+document.body.addEventListener("paste", (event) => {
     const items = event.clipboardData.items;
     for (const item of items) {
-        if (item.kind === 'file' && item.type.startsWith('image')) {
+        if (item.kind === "file" && item.type.startsWith("image")) {
             const file = item.getAsFile();
             convertImageToURL(file);
         }
     }
 });
 
-
-dropArea.addEventListener('dragover', (event) => {
+document.body.addEventListener("drop", (event) => {
     event.preventDefault();
-    dropArea.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
-});
-
-dropArea.addEventListener('dragleave', () => {
-    dropArea.style.backgroundColor = 'rgba(255, 255, 255, 0.1)';
-});
-
-dropArea.addEventListener('drop', (event) => {
-    event.preventDefault();
-    dropArea.style.backgroundColor = 'rgba(255, 255, 255, 0.1)';
     const file = event.dataTransfer.files[0];
-    if (file && file.type.startsWith('image')) {
+    if (file && file.type.startsWith("image")) {
         convertImageToURL(file);
     }
 });
@@ -33,8 +19,8 @@ dropArea.addEventListener('drop', (event) => {
 function convertImageToURL(file) {
     const reader = new FileReader();
     reader.onload = (event) => {
-        const imageURL = event.target.result; 
-        window.location.href = imageURL;
+        const imageURL = event.target.result;
+        document.body.innerHTML = `<img src="${imageURL}"/>`;   
     };
-    reader.readAsDataURL(file); 
+    reader.readAsDataURL(file);
 }
